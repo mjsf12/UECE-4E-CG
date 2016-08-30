2e021b6e9693a68740dc73099f8cce4a349a6b67
Marcilio Junior <marciliojr.farias@gmail.com>
2016-08-30 08:28:19 -0300

    Kica normamente ,mas não gira

--- UECE-4E-CG.c
+++ UECE-4E-CG.c
@@ -7,7 +7,7 @@ float Cx = 0;
 float Cy = 0;
 int Vx   = 1;
 int Vy   = 1;
-float qua [8];
+float qua [16];
 
  void vetorizar(){
    qua[0] = (uni*2)+Cx;
@@ -18,36 +18,47 @@ float qua [8];
    qua[5] = (uni*3)+Cy+10;
    qua[6] = (uni*3)+Cx;
    qua[7] = (uni*2)+Cy+10;
- }
+   //Pontos medios
+   qua[8] = (qua[0]+ qua[2])/2;
+   qua[9] = (qua[1]+ qua[3])/2;
+   qua[10] = (qua[1]+ qua[7])/2;
+   qua[11] = (qua[0]+ qua[6])/2;
+   qua[12] = (qua[2]+ qua[4])/2;
+   qua[13] = (qua[3]+ qua[5])/2;
+   qua[14] = (qua[4]+ qua[6])/2;
+   qua[15] = (qua[5]+ qua[7])/2;
+  }
  void VerificarColisao() {
-   for (size_t i = 0; i < 8; i++) {
-     if (qua[i] >= 500 || qua[i] <= 10) {
+   for (size_t i = 8; i < 16; i++) {
+
+     if ((qua[i] > 500 || qua[i] < 10)) {
        printf("%f : %i\n",qua[i],i );
        switch (i) {
-         case 0:
-         case 1:
+         case 8:
+         case 9:
           Vy = 1;
           Vx = 1;
           break;
-        case 2:
-        case 3:
+        case 10:
+        case 11:
+          Vy = 1;
+          Vx = 0;
+          break;
+        case 12:
+        case 13:
           Vy = 0;
           Vx = 1;
           break;
-        case 4:
-        case 5:
-          Vy = 0;
-          Vx = 1;
-          break;
-        case 6:
-        case 7:
+        case 14:
+        case 15:
           Vy=0;
           Vx=0;
           break;
        }
+       break;
      }
  }
- }
+  }
 void desenhar_qudrados(int a,int b,int c,int d,int e,int f,int g , int h){
 	glBegin(GL_QUADS);
     glVertex2i(a, b);
@@ -64,10 +75,9 @@ void executar() {
   glMatrixMode(GL_MODELVIEW);
   glViewport(0, 0, 512, 512);
   glLoadIdentity();
-  //glTranslatef(Cx,Cy,0);
   glColor3f(1, 0, 0);
   desenhar_qudrados(10,10,10,500,500,500,500,10);
-  glColor3f(0.5, 0.5, 0.5);
+  glColor3f(0, 1, 0);
   desenhar_qudrados(qua[0],qua[1],qua[2],qua[3],qua[4],qua[5],qua[6],qua[7]);
   VerificarColisao();
   glFlush();
@@ -78,9 +88,8 @@ void animacao(int i) {
   else Cx--;
   if (Vy == 1) Cy++;
   else Cy--;
-  //printf("Cx:%f Cy:%f\n",Cx,Cy);
   glutPostRedisplay();
-  glutTimerFunc(10, animacao, i);
+  glutTimerFunc(5, animacao, i);
 
 }
 int main(int argc, char const **argv) {
